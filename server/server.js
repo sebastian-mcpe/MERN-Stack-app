3d2a3982bec59dc3c0d9394a063904a3d8e18bdc
sebastian-mcpe <78837195+sebastian-mcpe@users.noreply.github.com>
2024-03-15 09:28:41 -0400

    feat: Created express server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import { config } from "dotenv";
+config({ path: "./server/config.env"});
+console.log(process.env.ATLAS_URI);
+
+import express from "express";
+import cors from "cors";
+import records from "./routes/record.js";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/record", records);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
